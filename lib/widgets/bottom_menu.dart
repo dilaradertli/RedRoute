e2df07245144846c92040b44ddf2ac4a6c55f756
CodeWithDilara <dertlidilara5@gmail.com>
2025-01-30 00:29:07 +0300

    Create bottom_menu.dart
    bottom menu eklendi

---
+++ lib/widgets/bottom_menu.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class BottomMenu extends StatelessWidget {
+  const BottomMenu({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 70,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          IconButton(
+            onPressed: () {
+              context.go("/home");
+            },
+            icon: Icon(
+              CupertinoIcons.home,
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              context.go("/favorite");
+            },
+            icon: Icon(
+              CupertinoIcons.heart,
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              context.go("/search");
+            },
+            icon: Icon(
+              Icons.search,
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              context.go("/chat");
+            },
+            icon: Icon(
+              CupertinoIcons.chat_bubble,
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              context.go("/profile");
+            },
+            icon: Icon(
+              CupertinoIcons.person,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
