b8d0e87961b1d9cdd2d438ccf7bcfa026563091a
CodeWithDilara <dertlidilara5@gmail.com>
2025-01-30 00:25:20 +0300

    Update routes.dart
    Yeni rota eklendi ve düzenlemeler yapıldı

--- lib/core/routes.dart
+++ lib/core/routes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/screens/settings_screen.dart';
 import 'package:go_router/go_router.dart';
 import '../screens/loading_screen.dart';
 import '../screens/home_screen.dart';
@@ -7,33 +8,57 @@ import '../screens/search_screen.dart';
 import '../screens/chat_screen.dart';
 import '../screens/favorite_screen.dart';
 
-// Router yapılandırması
 final router = GoRouter(
-  initialLocation: '/', // Başlangıç rotası
+  initialLocation: '/',
   routes: [
     GoRoute(
       path: '/',
-      builder: (context, state) => const LoadingScreen(),
+      pageBuilder: (context, state) => NoTransitionPage<void>(
+        key: state.pageKey,
+        child: const LoadingScreen(),
+      ),
     ),
     GoRoute(
       path: '/home',
-      builder: (context, state) => const HomeScreen(),
+      pageBuilder: (context, state) => NoTransitionPage<void>(
+        key: state.pageKey,
+        child: const HomeScreen(),
+      ),
     ),
     GoRoute(
       path: '/profile',
-      builder: (context, state) => const ProfileScreen(),
+      pageBuilder: (context, state) => NoTransitionPage<void>(
+        key: state.pageKey,
+        child: const ProfileScreen(),
+      ),
     ),
     GoRoute(
       path: '/search',
-      builder: (context, state) => const SearchScreen(),
+      pageBuilder: (context, state) => NoTransitionPage(
+        key: state.pageKey,
+        child: const SearchScreen(),
+      ),
     ),
     GoRoute(
       path: '/chat',
-      builder: (context, state) => const ChatScreen(),
+      pageBuilder: (context, state) => NoTransitionPage(
+        key: state.pageKey,
+        child: const ChatScreen(),
+      ),
     ),
     GoRoute(
       path: '/favorite',
-      builder: (context, state) => const FavoriteScreen(),
+      pageBuilder: (context, state) => NoTransitionPage(
+        key: state.pageKey,
+        child: const FavoriteScreen(),
+      ),
+    ),
+    GoRoute(
+      path: '/settings',
+      pageBuilder: (context, state) => NoTransitionPage(
+        key: state.pageKey,
+        child: const SettingsScreen(),
+      ),
     ),
   ],
 );
